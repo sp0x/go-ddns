a2ce38b30794050e701f5b4bba53c9af7264e91b
Vasil <vaskovasilev94@yahoo.com>
2020-07-26 19:27:37 +0200

    chore(ip): cleanup

--- rest-api/request_handler.go
+++ rest-api/request_handler.go
@@ -29,7 +29,6 @@ type WebserviceResponse struct {
 
 func BuildWebserviceResponseFromRequest(r *http.Request, appConfig *Config, extractors RequestDataExtractor) WebserviceResponse {
 	response := WebserviceResponse{}
-
 	sharedSecret := extractors.Secret(r)
 	response.Domains = strings.Split(extractors.Domain(r), ",")
 	response.Address = extractors.Address(r)
@@ -72,8 +71,6 @@ func BuildWebserviceResponseFromRequest(r *http.Request, appConfig *Config, extr
 			log.Println(fmt.Sprintf("Invalid address: %q", r.RemoteAddr))
 			return response
 		}
-
-		// @todo refactor this code to remove duplication
 		if ipparser.ValidIP4(ip) {
 			response.AddrType = "A"
 		} else if ipparser.ValidIP6(ip) {
@@ -110,15 +107,12 @@ func getRequestRemoteAddress(r *http.Request) (string, error) {
 	return "", errors.New("no match")
 }
 
-//ipRange - a structure that holds the start and end of a range of ip addresses
 type ipRange struct {
 	start net.IP
 	end   net.IP
 }
 
-// inRange - check to see if a given ip address is within a range given
-func inRange(r ipRange, ipAddress net.IP) bool {
-	// strcmp type byte comparison
+func networkIsInRange(r ipRange, ipAddress net.IP) bool {
 	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
 		return true
 	}
@@ -152,14 +146,13 @@ var privateAddressRanges = []ipRange{
 	},
 }
 
-// addressIsPrivate - check to see if this ip is in a private subnet
 func addressIsPrivate(ipAddress net.IP) bool {
 	// my use case is only concerned with ipv4 atm
 	if ipCheck := ipAddress.To4(); ipCheck != nil {
 		// iterate over all our ranges
 		for _, r := range privateAddressRanges {
 			// check if this ip is in a private range
-			if inRange(r, ipAddress) {
+			if networkIsInRange(r, ipAddress) {
 				return true
 			}
 		}
