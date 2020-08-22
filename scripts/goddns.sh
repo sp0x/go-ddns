0f6a6645637f89db74ac550e72788e96640f544d
Vasil <vaskovasilev94@yahoo.com>
2020-08-22 20:38:23 +0200

    added an example of goddns bash script for local ip

--- scripts/goddns.sh
+++ scripts/goddns.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 curl "${ENDPOINT}?secret=${API_KEY}&domain=${SUBDOMAIN}"
+
+#If you also need your local address to be added
+#install the npm module internal-ip-cli and execute
+#curl "${ENDPOINT}?secret=${API_KEY}&domain=local.${SUBDOMAIN}&addr=$(internal-ip --ipv4)"
