61061deb95ea8b5fedc23bda740b917f7054b014
Vasil Vasilev <vaskovasilev94@yahoo.com>
2020-11-02 22:32:50 +0100

    Update goddns.sh

--- scripts/goddns.sh
+++ scripts/goddns.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Use this script to make calls to a cloud function.
+# This way it's really easy update your records
+
+# Domain can be a FQDN or just a partial name.
 curl "${ENDPOINT}?secret=${API_KEY}&domain=${SUBDOMAIN}"
 
 #If you also need your local address to be added
